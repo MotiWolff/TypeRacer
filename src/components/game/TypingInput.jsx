cec71b0f0e75d50054ef61186f27e78224bfabd4
MotiWolff <motiwolff@gmail.com>
2025-10-23 12:28:54 +0300

    Refactor TypingInput component to use MUI TextField and accept value/onChange/disabled props

--- src/components/game/TypingInput.jsx
+++ src/components/game/TypingInput.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { TextField } from "@mui/material";
 
-export default function TypingInput() {
+export default function TypingInput({ value, onChange, disabled }) {
     return (
-        <div>TypingInput</div>
-    )
+        <TextField
+            fullWidth
+            variant="outlined"
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            placeholder="Start typing..."
+            autoFocus
+            disabled={disabled}
+            sx={{
+                "& .MuiOutlinedInput-root": {
+                    fontFamily: "monospace",
+                    fontSize: "1.1rem",
+                },
+            }}
+        >
+        </TextField>
+    );
 }
